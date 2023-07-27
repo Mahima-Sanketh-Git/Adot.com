6e0cdd9ff28594be1b82ceb36d20846630f0afc5
Mahima-Sanketh-Git <mahimadevinfo@gmail.com>
2023-07-27 15:41:47 +0530

    Adot.com

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -49,15 +49,108 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            child: SizedBox(
+              height: 117,
+              width: 390,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Image.asset("assets/now1.jpg"),
+                  Image.asset("assets/now2.jpg"),
+                  Image.asset("assets/now3.jpg"),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color.fromRGBO(226, 234, 242, 100),
+                  ),
+                  child: Image.asset("assets/Laptop.png"),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color.fromRGBO(226, 234, 242, 100),
+                  ),
+                  child: Image.asset("assets/Watermelon.png"),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color.fromRGBO(226, 234, 242, 100),
+                  ),
+                  child: Image.asset("assets/bb.png"),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color.fromRGBO(226, 234, 242, 100),
+                  ),
+                  child: Image.asset("assets/Tricycle.png"),
+                ),
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color.fromRGBO(226, 234, 242, 100),
+                  ),
+                  child: Image.asset("assets/Armchair.png"),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Laptop"),
+                Text("Food"),
+                Text("Fashion"),
+                Text("Toys"),
+                Text("Home"),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20),
             child: Row(
               children: [
-                Image.asset(
-                  'assets/baner1.png',
-                  height: 117,
-                  width: 390,
+                Text(
+                  "Flash Sale",
+                  style: GoogleFonts.poppins(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
+                Image.asset("assets/Fire.png")
               ],
             ),
           ),
